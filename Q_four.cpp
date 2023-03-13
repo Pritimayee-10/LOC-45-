935cad652cba0a449a7a741767047977a4952f28
Pritimayee-10 <126600827+Pritimayee-10@users.noreply.github.com>
2023-03-13 21:55:08 +0530

    Update Q_four.cpp

--- Q_four.cpp
+++ Q_four.cpp
@@ -1,5 +1,3 @@
-/*Write a C program to enter two numbers and perform all arithmetic
-operations using switch case.*/
 #include<stdio.h>
 int main()
 {
@@ -7,19 +5,15 @@ int main()
 	printf("Enter two number:");
 	scanf("%d %d",&num1,&num2);
 	switch(1){
-		case '+':
+		case 1:
 		printf("%d+%d=%d",num1,num2,(num1+num2));
-		case '-':
+		case 2:
 		printf("%d-%d=%d",num1,num2,(num1-num2));	
-		case 'X':
+		case 3:
 		printf("%dX%d=%d",num1,num2,(num1*num2));
-		case 'x':
-		printf("%dX%d=%d",num1,num2,(num1*num2));
-		/*added two cases for multiplication so that user can conveniatily perform 
-		the task and doesn't get confused in small x and capital X */
-		case '/':
+		case 4:
 		printf("%d/%d=%d",num1,num2,(num1/num2)); 
-		case '%':
+		case 5:
 		printf("%d%%%d=%d",num1,num2,(num1%num2)); //if we will will write %%d it will print %d so we have write %%%d	
 	}
 	return 0;
